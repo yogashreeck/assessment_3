0b7d49f21789e3097680d45794aa4ab6f64a421d
yogashreeck <ykumarostb1@gmail.com>
2019-06-02 23:49:39 +0530

    validation done

--- javascript/login.js
+++ javascript/login.js
@@ -1,9 +1,9 @@
-// popup login form
+//to popup login form...
 function openForm() {
 	document.getElementById("myForm").style.display = "block";
 }
 
-// register form
+// register form validation..
 
 function singValidation() {
 
@@ -23,28 +23,61 @@ function singValidation() {
 	var fnamecheck = /^[A-Za-z. ]{1,15}$/ ;
 	var lnamecheck = /^[A-Za-z. ]{1,15}$/ ;
 	var emailcheck = /^[A-Za-z. ]{1,15}$/ ;
-	var pnumbercheck = /^[0-9]{10}$/ ;
+	var pnumbercheck = /^[]{10}$/ ;
 	var street1check = /^[A-Za-z. ]{1,15}$/ ;
 	var street2check = /^[A-Za-z. ]{1,15}$/ ;
 	var citycheck = /^[A-Za-z. ]{1,15}$/ ;
 	var statecheck = /^[A-Za-z. ]{1,15}$/ ;
 	var zipcodecheck = /^[A-Za-z. ]{1,15}$/ ;
 	var passcheck = /^[A-Za-z. ]{1,15}$/ ;
-	var vpassecheck = /^[A-Za-z. ]{1,15}$/ ;
+	var vpasscheck = /^[A-Za-z. ]{1,15}$/ ;
 
 	if (fnamecheck.test(fname)) {
 		document.getElementById('fnamerror').innerHTML = " ";
 	}
-	else{
-		document.getElementById('fnamerror').innerHTML = " First name cannot be blank" ;
-		return false;
-	}
-
-	if (lnamecheck.test(lname)) {
+    if (lnamecheck.test(lname)) {
 		document.getElementById('lnamerror').innerHTML = " ";
 	}
+	if (emailcheck.test(email)) {
+		document.getElementById('emailerror').innerHTML = " ";
+	}
+	if (pnumbercheck.test(pnumber)) {
+		document.getElementById('pnumerror').innerHTML = " ";
+	}
+	if ( street1check.test(Street1)) {
+		document.getElementById('street1error').innerHTML = " ";
+	}
+	if ( street2check.test(Street2)) {
+		document.getElementById('street2error').innerHTML = " ";
+	}
+	if (citycheck.test(city)) {
+		document.getElementById('cityerror').innerHTML = " ";
+	}
+	if (statecheck.test(state)) {
+		document.getElementById('stateerror').innerHTML = " ";
+	}
+	if (zipcodecheck.test(zipcode)) {
+		document.getElementById('ziperror').innerHTML = " ";
+	}
+	if (passcheck.test(password)) {
+		document.getElementById('passerror').innerHTML = " ";
+	}
+	if (vpasscheck.test(vpassword)) {
+		document.getElementById('vpasserror').innerHTML = " ";
+	}
+
 	else{
+		document.getElementById('fnamerror').innerHTML = " First name cannot be blank" ;
 		document.getElementById('lnamerror').innerHTML = " last name cannot be blank" ;
+		document.getElementById('emailerror').innerHTML = "we need your email address to contact you";
+		document.getElementById('pnumerror').innerHTML = " phone number cannot be blank";
+		document.getElementById('street1error').innerHTML = "street cannot be blank";
+		document.getElementById('street2error').innerHTML = "street address cannot be blank";
+		document.getElementById('cityerror').innerHTML = "city cannot be blank";
+		document.getElementById('stateerror').innerHTML = "state can't be blank";
+		document.getElementById('ziperror').innerHTML = "zipcode can't be blank";
+		document.getElementById('passerror').innerHTML = "password cannot be blank";
+		document.getElementById('vpasserror').innerHTML = "verify password cannot be blank";
 		return false;
 	}
 }
