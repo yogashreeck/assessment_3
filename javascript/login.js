0df6c2ac65ae17277e51af1dc8d8dda2e6c3a85e
yogashreeck <ykumarostb1@gmail.com>
2019-06-08 16:57:54 +0530

    validation done

--- javascript/login.js
+++ javascript/login.js
@@ -6,6 +6,7 @@ function openForm() {
 // register form validation..
 
 function singValidation() {
+	debugger;
 
 	var fname = document.getElementById('fname').value;
 	var lname = document.getElementById('lname').value;
@@ -18,6 +19,18 @@ function singValidation() {
 	var zipcode = document.getElementById('zipcode').value;
 	var password = document.getElementById('password').value;
 	var vpassword = document.getElementById('vpassword').value;
+
+  	var  fname1 = document.getElementById('fname1');
+  	var lname1 = document.getElementById('lname1');
+	var email1 = document.getElementById('email1');
+	var pnumber1 = document.getElementById('pnumber1');
+	var Street11 = document.getElementById('Street11');
+	var Street21 = document.getElementById('Street21');
+	var city1 = document.getElementById('city1');
+	var state1 = document.getElementById('state1');
+	var zipcode1 = document.getElementById('zipcode1');
+	var password1 = document.getElementById('password1');
+	var vpassword1 = document.getElementById('vpassword1');
 
 
 	var fnamecheck = /^[A-Za-z. ]{1,15}$/ ;
@@ -68,16 +81,49 @@ function singValidation() {
 
 	else{
 		document.getElementById('fnamerror').innerHTML = " First name cannot be blank" ;
+		document.getElementById("fname").className = "wrong-error";
+		document.getElementById("fname1").className = "wname";
+
 		document.getElementById('lnamerror').innerHTML = " last name cannot be blank" ;
+		document.getElementById("lname").className = "wrong-error";
+		document.getElementById("lname1").className = "wname";
+
 		document.getElementById('emailerror').innerHTML = "we need your email address to contact you";
+		document.getElementById("email").className = "wrong-error";
+		document.getElementById("email1").className = "wname";
+
 		document.getElementById('pnumerror').innerHTML = " phone number cannot be blank";
+		document.getElementById("pnumber").className = "wrong-error";
+		document.getElementById("pnumber1").className = "wname";
+
 		document.getElementById('street1error').innerHTML = "street cannot be blank";
+		document.getElementById("Street1").className = "wrong-error";
+		document.getElementById("Street11").className = "wname";
+
 		document.getElementById('street2error').innerHTML = "street address cannot be blank";
+		document.getElementById("Street2").className = "wrong-error";
+		document.getElementById("Street21").className = "wname";
+
 		document.getElementById('cityerror').innerHTML = "city cannot be blank";
+		document.getElementById("city").className = "wcity";
+		document.getElementById("city1").className = "wname";
+
 		document.getElementById('stateerror').innerHTML = "state can't be blank";
+		document.getElementById("state").className = "wcity";
+		document.getElementById("state1").className = "wname";
+
 		document.getElementById('ziperror').innerHTML = "zipcode can't be blank";
+		document.getElementById("zipcode").className = "wcity";
+		document.getElementById("zipcode1").className = "wnames";
+
 		document.getElementById('passerror').innerHTML = "password cannot be blank";
+		document.getElementById("password").className = "wrong-error";
+		document.getElementById("password1").className = "wname";
+
 		document.getElementById('vpasserror').innerHTML = "verify password cannot be blank";
+		document.getElementById("vpassword").className = "wrong-error";
+		document.getElementById("vpassword1").className = "wname";
+
 		return false;
 	}
 }
